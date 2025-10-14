842239ca89f680fa0784599a1c1b281d87ad9573
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-14 10:50:10 -0300

    Interface do usuário construida

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,2 +1,13 @@
+export enum tipoUsuario {
+    FUNCIONARIO = "funcionário",
+    RESPONSAVEL = "responsável",
+    ALUNO = "aluno"
+}
+
 export interface Usuario {
+    id: string;
+    nome: string,
+    senha: string,
+    tipoUsuario: tipoUsuario,
+    codigo?: string,
 }
