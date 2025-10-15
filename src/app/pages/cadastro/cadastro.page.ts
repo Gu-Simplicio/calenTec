0632a6d3df59534042e699a96d07dca2147e4e1d
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-15 09:33:17 -0300

    usuário sendo criado no cadastro

--- src/app/pages/cadastro/cadastro.page.ts
+++ src/app/pages/cadastro/cadastro.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { tipoUsuario, Usuario } from 'src/app/models/usuario';
+import { Autenticacao } from 'src/app/services/autenticacao';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,10 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
+  //recebe o formulário de cadastro
+  @ViewChild('formularioCadastro') formularioCadastro!: NgForm;
 
-  constructor() { }
+  // salva os campos do usuário
+  usuario: Usuario = {
+    id: '',
+    nome: '',
+    email: '',
+    senha: '',
+    codigo: '',
+    tipoUsuario: tipoUsuario.ALUNO
+  }
+  //variável responsável por exibir (ou não) o campo de RM/código
+  mostrarCodigo!: boolean;
+
+  constructor(
+    private autenticacao: Autenticacao,
+    private router: Router
+  ) { 
+    this.alteraTipo() 
+  }
 
   ngOnInit() {
   }
 
+  // exibe ou não o campo de código/Rm
+  alteraTipo(): void{
+    this.mostrarCodigo = this.usuario.tipoUsuario !== tipoUsuario.RESPONSAVEL;
+  }
+
+  async cadastrar(){
+    //checa se o formulário é válido
+    if(!this.formularioCadastro.valid) throw new Error("Formulário incompleto!")
+    
+    //realiza o cadastro do usuário
+    try {
+      await this.autenticacao.cadastrar(this.usuario);
+
+      this.router.navigate(['/menu']);
+    }catch(erro){
+      console.error(`Erro ${erro}`);
+    }
+  }
 }
