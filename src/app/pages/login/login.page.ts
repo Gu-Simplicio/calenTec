87474ee22f33e7bdf597ff7b882edb6bb30d6e9f
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-15 15:02:25 -0300

    login funcional

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Autenticacao } from 'src/app/services/autenticacao';
 
 @Component({
@@ -8,12 +10,36 @@ import { Autenticacao } from 'src/app/services/autenticacao';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  //recebe o formulario de login
+  @ViewChild('formularioLogin') formularioLogin!: NgForm;
+  //prepara o objeto do usuário
+  usuario = { 
+    email: '',
+    senha: ''
+  }
 
   constructor(
-    private autenticacao: Autenticacao
+    private autenticacao: Autenticacao,
+    private router: Router
   ) { }
 
   ngOnInit() {
   }
 
+  async entrar(){
+    if(!this.formularioLogin.valid) throw new Error("Formulário inválido!!");
+
+    try {
+      const usuarioLogado = await this.autenticacao.login(this.usuario.email, this.usuario.senha)
+
+      //caso o usuário tenha sido logado corretamente
+      if(usuarioLogado){
+        this.router.navigate(['/menu']);
+      } else {
+        throw new Error("Erro ao entrar");
+      }
+    }catch(erro){
+      console.log(erro)
+    }
+  }
 }
