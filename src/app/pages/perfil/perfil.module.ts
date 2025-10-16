78382fe309c1d67904d91c13c62674af2fd70693
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-16 12:55:16 -0300

    footer importado para o perfil

--- src/app/pages/perfil/perfil.module.ts
+++ src/app/pages/perfil/perfil.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { PerfilPageRoutingModule } from './perfil-routing.module';
 
 import { PerfilPage } from './perfil.page';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PerfilPageRoutingModule
+    PerfilPageRoutingModule,
+    FooterComponent
   ],
   declarations: [PerfilPage]
 })
