cf1ae63632d0ba1648753407807287e2a09d2400
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-10-16 12:02:46 -0300

    typescript e html do footer estruturados

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -8,6 +8,7 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule]
 })
 export class FooterComponent  implements OnInit {
+  @Input() funcLogado?: boolean;
 
   constructor() { }
 
