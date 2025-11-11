7a0095cae36e5596d69514485dc53b78cba0b607
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-11-11 13:07:33 -0300

    12 calendários estão aparecendo no menu

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -9,14 +9,34 @@ import { Autenticacao } from 'src/app/services/auth/autenticacao';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
-  usuario!: Usuario | null
+  usuario!: Usuario | null;
 
-  constructor( private autenticacao: Autenticacao ) { }
+  //array que receberá os meses do ano
+  mesesDoAno: Date[] = [];
+
+  constructor( private autenticacao: Autenticacao ) {
+    this.gerarMeses();
+  }
 
   async ngOnInit() {
     this.usuario = await this.autenticacao.getUsuarioAtual();
 
     console.log(this.usuario)
+  }
+
+  gerarMeses(): void {
+    const anoAtual = new Date().getFullYear();
+
+    for(let i = 0; i < 12; i++){
+      //cria uma data para o dia 1 de cada mês e insere no array
+      const mesNovo = new Date(
+        anoAtual,  // ano atual
+        i,  // i = 0 é janeiro, i = 2 é fevereiro..
+        1 //dia 1 do mês escolhido
+      );
+
+      this.mesesDoAno.push(mesNovo);
+    }
   }
 
 }
