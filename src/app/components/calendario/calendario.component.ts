8fd7f3d35fed265e0787b3c46049084db7a91b32
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-11-11 08:04:36 -0300

    typescript do componente de calendário criado, agora tem que mostrar o calendário no menu do jeito certo

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import ptBrLocale from '@fullcalendar/core/locales/pt-br';
 
 @Component({
   selector: 'app-calendario',
@@ -9,16 +10,35 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   styleUrls: ['./calendario.component.scss'],
 })
 export class CalendarioComponent  implements OnInit {
+  //recebe o mês que deve ser exibido
+  @Input() dataInicial: Date = new Date();
+
+  // definindo as opções do calendário
    calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin],
+    locale: ptBrLocale,
+    // opções para travar o calendário
+    headerToolbar: { //remove botões 'prev', 'next' e 'today'
+      left: '',
+      center: 'title', //mantém o título
+      right: ''
+    },
+    //impede de editar e clicar nos dias e eventos
+    navLinks: false,
+    selectable: false,
+    editable: false,
+
     events: [
-      { title: 'Evento Teste', date: new Date() }
+      { title: "Evento de teste", date: new Date() }
     ]
   };
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    //inicia o calendário com a data inicial inserida
+    this.calendarOptions.initialDate = this.dataInicial;
+  }
 
 }
