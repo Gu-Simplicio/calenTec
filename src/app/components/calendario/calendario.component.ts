5616dfbcc07c23e0c0b5b5ec88ee1fe4450dd808
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-11-12 00:34:01 -0300

    componente do calendário preparado com o service pra salvar tudo no storage também

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { CalendarOptions, EventApi, EventClickArg } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 import ptBrLocale from '@fullcalendar/core/locales/pt-br';
 import { AlertController } from '@ionic/angular';
+import { Eventos } from 'src/app/services/eventos/eventos';
+import { EventoCalendario } from 'src/app/models/EventoCalendario';
+import { FullCalendarComponent } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-calendario',
@@ -16,6 +19,8 @@ export class CalendarioComponent  implements OnInit {
   @Input() dataInicial: Date = new Date();
   //recebe se o usuário é um funcionário ou não
   @Input() ehFuncionario: boolean | undefined | null;
+  // recebe o componente do calendário
+  @ViewChild('calendario') componenteCalendario!: FullCalendarComponent;
 
   // definindo as opções do calendário
    calendarOptions: CalendarOptions = {
@@ -33,16 +38,27 @@ export class CalendarioComponent  implements OnInit {
     selectable: false,
     editable: false,
 
-    events: [
-      { title: "Evento de teste", date: new Date() }
-    ]
+    eventTimeFormat: { //indicando para exibir horário 
+      hour: '2-digit',
+      minute: '2-digit',
+      meridiem: false,
+      hour12: false
+    },
+    displayEventTime: true, // garante que o horário seja exibido
+    events: [] // inicia os eventos sem nada
   };
 
-  constructor( private alertController: AlertController ) { }
+  constructor( 
+    private eventosService: Eventos,
+    private alertController: AlertController
+   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     //inicia o calendário com a data inicial inserida
     this.calendarOptions.initialDate = this.dataInicial;
+
+    // carrega os eventos salvos no Storage
+    this.calendarOptions.events = await this.eventosService.getEventos();
 
     //no caso de ser funcionário
     if(this.ehFuncionario === true){
@@ -62,10 +78,15 @@ export class CalendarioComponent  implements OnInit {
       header: "Novo evento",
       message: `Criar evento para ${arg.dateStr}`,
       inputs: [ //cria um input pro evento 
-        {
+        { //input de título do evento
           name: 'titulo',
           type: 'text',
           placeholder: 'Nome do evento',
+        },
+        { //input de horário do evento
+          name: 'horario',
+          type: 'time', // input de horário
+          placeholder: "HH:mm"
         }
       ],
       buttons: [ //botões de salvar e cancelar
@@ -75,13 +96,23 @@ export class CalendarioComponent  implements OnInit {
         },
         {
           text: 'Salvar',
-          handler: (data) => { //botão de salvar cria o evento 
-            if(data.titulo){
-              arg.view.calendar.addEvent({
+          handler: async (data) => { //botão de salvar cria o evento 
+            if(data.titulo && data.horario){
+              // constrói a maneira ISO da data
+              const dataISO = `${arg.dateStr}T${data.horario}:00`;
+
+              const novoEvento: EventoCalendario = {
+                id: Date.now().toString(), //gera um id baseado no momento em que o evento foi criado
                 title: data.titulo,
-                start: arg.dateStr,
-                allDay: true
-              });
+                start: dataISO,
+                allDay: false // diz que o evento não necessariamente dura o dia todo
+              }
+
+              //salva o evento na UI
+              arg.view.calendar.addEvent(novoEvento);
+
+              // salva o evento no Storage
+              await this.salvarTodosEventosDaUI(arg.view.calendar);
             }
           }
         }
@@ -130,8 +161,11 @@ export class CalendarioComponent  implements OnInit {
         },
         { // CONFIRMA A AÇÃO
           text: 'Remover',
-          handler: () => {
+          handler: async () => {
             event.remove(); //remove o evento da UI
+
+            // salva no storage
+            await this.salvarTodosEventosDaUI(this.componenteCalendario.getApi());
           }
         }
       ]
@@ -143,14 +177,23 @@ export class CalendarioComponent  implements OnInit {
 
   // UPDATE do calendário
   async editarEvento(event: EventApi){
+    //formatar a hora autal do evento
+    // (ex.: de "2025-10-10T09:00:00" vai para "09:00")
+    const horaAtual = event.start?.toTimeString().split(' ')[0].substring(0, 5)
+
     const alert = await this.alertController.create({
       header: "Editar evento",
       inputs: [ // dá a opção de editar os dados inseridos
-        {
+        { // input do título
           name: 'titulo',
           type: 'text',
           value: event.title, //já vem com o título atual
           placeholder: 'Novo nome do evento'
+        },
+        {
+          name: 'horario',
+          type: 'time',
+          value: horaAtual
         }
       ],
       buttons: [ // botões de ação
@@ -160,9 +203,20 @@ export class CalendarioComponent  implements OnInit {
         },
         { // SALVA A EDIÇÃO
           text: 'Salvar',
-          handler: (data) => {
-            if(data.titulo) { 
-              event.setProp('title', data.titulo);
+          handler: async (data) => {
+            if(data.titulo && data.horario) { 
+              // pega a data original sem o horário
+              const dataOriginal = event.startStr.split('T')[0];
+              //cria a data com horário atualizado
+              const novoStartISO = `${dataOriginal}T${data.horario}:00`;
+
+              //atualiza a UI
+              event.setProp('title', data.titulo); //atualiza título
+              event.setStart(novoStartISO); //atualiza data/hora
+              event.setProp('allDay', false); // diz que não dura o dia todo
+
+              // salva no storage
+              await this.salvarTodosEventosDaUI(this.componenteCalendario.getApi());
             }
           }
         }
@@ -170,5 +224,22 @@ export class CalendarioComponent  implements OnInit {
     });
 
     await alert.present();
+  }
+
+  // método para salvar todos os eventos da UI no storage
+  private async salvarTodosEventosDaUI(calendarAPI: any){
+    // pega todos os eventos que estão no calendário
+    const eventosAtuais = calendarAPI.getEvents();
+
+    //converte para o formato PlainObject (pra poder ser salvo com JSON) e mapeia a interface eventoCalendario
+    const eventosParaSalvar: EventoCalendario[] = eventosAtuais.map((e: EventApi) => ({
+      id: e.id || new Date().toString(), //garante um id
+      title: e.title,
+      start: e.startStr,
+      allDay: e.allDay
+    }));
+
+    // salva a lista completa com o service
+    await this.eventosService.salvarEventos(eventosParaSalvar);
   }
 }
