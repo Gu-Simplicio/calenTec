57fd9bbf147959c8c980f0ffa6c4ccec8ea0e3bf
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-11-11 23:48:35 -0300

    Service dos eventos criado

--- src/app/services/eventos/eventos.ts
+++ src/app/services/eventos/eventos.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { EventoCalendario } from 'src/app/models/EventoCalendario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Eventos {
-  
+  //chave para ler os eventos salvos
+  private readonly CHAVE_EVENTOS = "eventos_calentec";
+
+  constructor( private storage: Storage ) {
+    this.storage.create(); //garante que o storage está pronto
+  }
+
+  // retorna todos os eventos salvos
+  async getEventos(): Promise<EventoCalendario[]>{
+    const eventos = await this.storage.get(this.CHAVE_EVENTOS) || [];
+
+    return eventos;
+  }
+
+  // salva a lista completa de eventos no storage
+  async salvarEventos(eventos: EventoCalendario[]): Promise<any>{
+    return this.storage.set(this.CHAVE_EVENTOS, eventos);
+  }
 }
